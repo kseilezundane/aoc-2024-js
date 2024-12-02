6bfd25eed1ab39f2fbbee53fc46592ba299752b7
Karina <karina.seile.zundane@gmail.com>
2024-12-02 19:56:03 +0200

    Solve December 2nd tasks

---
+++ 02.12/2-1.js
@@ -0,0 +1,41 @@
+"use strict";
+
+import createReadlineInterface from "../utils/create-readline-interface.js";
+
+// checks if array is ascending and difference between neighbors is not bigger than 3
+const isAscending = (array) => {
+    return array.every((currentValue, index) => {
+        if (index === array.length - 1) {
+            return true;
+        }
+        return (currentValue < array[index + 1]) && (Math.abs(currentValue - array[index + 1]) <= 3);
+    });
+};
+
+// checks if array is descending and difference between neighbors is not bigger than 3
+const isDescending = (array) => {
+    return array.every((currentValue, index) => {
+        if (index === array.length - 1) {
+            return true;
+        }
+        return currentValue > array[index + 1] && (Math.abs(currentValue - array[index + 1]) <= 3);
+    });
+};
+
+async function findSafeReports() {
+    const readlineInterface = createReadlineInterface("input.txt");
+
+    let safeReports = 0;
+    for await (const line of readlineInterface) {
+        let reportValues = line.split(" ");
+        reportValues = reportValues.map((element) => Number(element));
+        if (isDescending(reportValues) || isAscending(reportValues)) {
+            safeReports++;
+        }
+    }
+
+    console.log(safeReports);
+}
+
+await findSafeReports();
+
---
+++ 02.12/2-2.js
@@ -0,0 +1,45 @@
+"use strict";
+
+import createReadlineInterface from "../utils/create-readline-interface.js";
+
+// checks if array is ascending and difference between neighbors is not bigger than 3
+const isAscending = (array) => {
+    return array.every((currentValue, index) => {
+        if (index === array.length - 1) {
+            return true;
+        }
+        return (currentValue < array[index + 1]) && (Math.abs(currentValue - array[index + 1]) <= 3);
+    });
+};
+
+// checks if array is descending and difference between neighbors is not bigger than 3
+const isDescending = (array) => {
+    return array.every((currentValue, index) => {
+        if (index === array.length - 1) {
+            return true;
+        }
+        return currentValue > array[index + 1] && (Math.abs(currentValue - array[index + 1]) <= 3);
+    });
+};
+
+async function findSafeReports() {
+    const readlineInterface = createReadlineInterface("input.txt");
+
+    let safeReports = 0;
+    for await (const line of readlineInterface) {
+        let reportValues = line.split(" ");
+        reportValues = reportValues.map((element) => Number(element));
+        // dropping each number from a report to check if it becomes safe without one of them
+        for (let i = 0; i < reportValues.length; i++) {
+            const newValues = reportValues.toSpliced(i, 1);
+            if (isDescending(newValues) || isAscending(newValues)) {
+                safeReports++;
+                break;
+            }
+        }
+    }
+
+    console.log(safeReports);
+}
+
+await findSafeReports();
